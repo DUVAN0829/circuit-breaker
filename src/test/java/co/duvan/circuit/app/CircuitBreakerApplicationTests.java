57ee1a01efa92dd0e4613ce3604115a082f2faac
DUVAN0829 <duvthan067@gmail.com>
2026-01-10 17:04:59 -0500

    Primer commit :D

---
+++ src/test/java/co/duvan/circuit/app/CircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.duvan.circuit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
